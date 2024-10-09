c106d45e2a6d9cb8d940a98f23baa3372195f54f
nowcaz <127193137+nowcaz@users.noreply.github.com>
2024-10-09 11:52:03 +0300

    Add files via upload

---
+++ Selenium/Selenium/FirstTestCase.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium
+{
+    public class FirstTestCase
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "https://store.steampowered.com/";
+        }
+    }
+}
